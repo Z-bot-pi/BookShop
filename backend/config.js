a2ed86833a585e7fc549cd52ae358fc30f35ae5e
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-01 21:55:07 +0000

    mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 3000;
 
-export const mongoDBURL = 
-'mongodb+srv://xulubanazintle:<db_password>@cluster0.5afjakh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+export const mongoDBURL = 'mongodb+srv://xulubanazintle:xulubanazintle@book-shop.g3bky.mongodb.net/?retryWrites=true&w=majority&appName=Book-Shop'
