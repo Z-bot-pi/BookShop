4d0e242d013a6ac198d693a74ded379b5571960b
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-01 18:49:06 +0000

    port

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
-import express from "express";
-import { PORT } from "./config";
+import express, { request, response } from "express";
+import { PORT } from './config.js';
 const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to Book Shop');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
