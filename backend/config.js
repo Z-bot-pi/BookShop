408a47496c1a9d56b7f5572d84a034cda4354cc7
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-07 10:57:52 +0000

    backend

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 3000;
 
-export const mongoDBURL = 'mongodb+srv://xulubanazintle:xulubanazintle@book-shop.g3bky.mongodb.net/?retryWrites=true&w=majority&appName=Book-Shop'
+export const mongoDBURL = 'mongodb+srv://xulubanazintle:xulubanazintle@book-shop.g3bky.mongodb.net/?retryWrites=true&w=majority&appName=Book-Shop';
