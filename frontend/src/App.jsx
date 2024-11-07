b6d91982afa74a0aa629019e389264ccf73d0484
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-07 18:11:22 +0000

    screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <nav>Nabar</nav>
-    <main className='min-h-screen'>
+    <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6'>
       <Outlet />
     </main>
       <footer>Footer</footer>
