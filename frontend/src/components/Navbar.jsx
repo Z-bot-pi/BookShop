1dda6bfb97adf75634b6e61a25440f54fc667aeb
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-08 01:19:10 +0000

    Nav bar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+
+const Navbar = () => {
+  return (
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+<nav>
+    {/*Left*/}
+    <div>Logo and Search bar</div>
+
+    {/*Right*/}
+    <div>
+        nav items
+    </div>
+</nav>
+    </header>
+  )
+}
+
+export default Navbar
