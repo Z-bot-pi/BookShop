e181014937fc7c4f7907e8e8cf3eab04f49e3d21
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-13 21:30:10 +0000

    Login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='h-[calc(100vh-120px)] border'>
+        <div>
+            <h2 className='text-xl font-semibold mb-4'>Kindly Login</h2>
+        </div>
+    </div>
   )
 }
