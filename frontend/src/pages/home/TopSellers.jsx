27ac3400d617541200956b7851d67f7a6a433259
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-13 14:48:47 +0000

    category

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+const categories = ['Choose a genre', 'Horror','Fiction','Adventure','Business']
+
 const TopSellers = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('books.json').
+        then(res => res.json())
+        .then((data) => setBooks(data))
+        } , [])
+
+
+
   return (
-    <div>TopSellers</div>
+    <div className='py-10'>
+        <h2 className='text-3xl font-semibold mb-6'>Top selling books</h2>
+        {/*Categorizing filter*/}
+        <div>
+            <select name='category' id='category'>
+                {
+                    categories.map((category, index) => (
+                        <option key={index} value={category}>{category}</option>
+                    ))
+                }
+            </select>
+        </div>
+    </div>
   )
 }
 
