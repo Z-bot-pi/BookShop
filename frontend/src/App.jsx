b5aef96d950241bd5b4f3734a68ae3994743bc6f
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-13 20:56:27 +0000

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
       <Outlet />
     </main>
-      <footer>Footer</footer>
+      <Footer/>
     </>
   )
 }
