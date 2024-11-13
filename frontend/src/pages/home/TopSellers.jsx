a142090a44c2774ed4535b48194e62f3a705fedc
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-13 15:33:53 +0000

    top sellers

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -4,6 +4,7 @@ const categories = ['Choose a genre', 'Horror', 'Fiction', 'Adventure', 'Busines
 
 const TopSellers = () => {
     const [books, setBooks] = useState([]); // Store the fetched books data
+    const [selectedCategory, setSelectedCategory] = useState('Choose a genre'); // Store the selected category
 
     useEffect(() => {
         // Fetch data from books.json
@@ -12,13 +13,19 @@ const TopSellers = () => {
             .then((data) => setBooks(data)); // Set the books data to state
     }, []); // Empty dependency array, so it runs only once when the component mounts
 
+const filteredBooks =  selectedCategory === 'Choose a genre' ? books : books.filter(book => book.category === selectedCategory.toLowerCase())
+
+console.log(filteredBooks);
+
     return (
         <div className="py-10">
             <h2 className="text-3xl font-semibold mb-6">Top selling books</h2>
             
             {/* Categorizing filter */}
             <div className='mb-8 flex items-center'>
-                <select name="category" id="category" className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
+                <select 
+                onChange={(e) => setSelectedCategory(e.target.value)}
+                name="category" id="category" className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
                     {
                     categories.map((category, index) => (
                         <option key={index} value={category}>
@@ -28,6 +35,12 @@ const TopSellers = () => {
                     }
                 </select>
             </div>
+
+            {
+                filteredBooks.map((book, index) => (
+                    <div>{book.title}</div>
+                ))
+            }
       </div>
     )
 }
