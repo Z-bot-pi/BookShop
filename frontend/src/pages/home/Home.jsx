39eba60afeffa588e9ffcf0b617f62e5c4826d63
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-13 14:20:15 +0000

    book list

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Banner from './Banner'
+import TopSellers from './TopSellers'
 
 export const Home = () => {
   return (
     <>
+     <TopSellers/>
     <Banner/>
+   
     </>
   )
 }
