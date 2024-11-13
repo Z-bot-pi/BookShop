e8ba7104b043363fa31c71968a57a7033bbb2e89
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-13 13:56:46 +0000

    banner

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
         {/* Left Container with Heading and Image */}
         <div className="md:w-1/2 w-full flex flex-col items-center md:items-start">
           <h1 className="md:text-4xl text-2xl font-medium mb-2">This week's new release</h1> {/* Reduced bottom margin */}
-          <img src={bannerImg} alt="Banner" className="w-1/2 mb-2" /> {/* Removed extra space below */}
+          <img src={bannerImg} alt="Banner" className="w-1/2 mb-2 flex items-center md:justify-end" /> {/* Removed extra space below */}
         </div>
         
         {/* Right Container with Paragraph and Button */}
