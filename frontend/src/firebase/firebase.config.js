fa0d6732e023a77b4eba8ceb99bf8e113628e8a6
Zintle Mazibuko <xulubanazintle@gmail.com>
2024-11-14 01:09:33 +0000

    firebase

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -1,19 +1,21 @@
-// Import the functions you need from the SDKs needed
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_Auth_Domain,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyBNPm7n45A6zGsQVARjp04N6VQ91-rLqeM",
+  authDomain: "bookshop-5cc00.firebaseapp.com",
+  projectId: "bookshop-5cc00",
+  storageBucket: "bookshop-5cc00.firebasestorage.app",
+  messagingSenderId: "381547777817",
+  appId: "1:381547777817:web:1992447331dcc7f9013f88",
+  measurementId: "G-V64NN9LGEN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth =  getAuth(app);
+const analytics = getAnalytics(app);
